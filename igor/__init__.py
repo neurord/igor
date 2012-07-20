5375c9477d12aaf0b83e7d0d0bf0873c475c7e64
W. Trevor King <wking@tremily.us>
2012-07-20 14:46:08 -0400

    Add igor.LOG for easier debugging.

--- igor/__init__.py
+++ igor/__init__.py
@@ -3,3 +3,13 @@
 "Interface for reading binary IGOR files."
 
 __version__ = '0.2'
+
+
+import logging as _logging
+
+
+LOG = _logging.getLogger('igor')
+LOG.setLevel(_logging.ERROR)
+LOG.addHandler(_logging.StreamHandler())
+LOG.handlers[-1].setFormatter(
+    _logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
